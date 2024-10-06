5ce0d3ceb401bf8f9d99ebc4f960b801e3cf8ed2
Roman Gravit <romang@siber.com>
2024-10-06 15:43:35 +0100

    Prepare for lesson13

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,4 @@
-import { ProjectInput } from "./components/project-input.js";
-import { ProjectList } from "./components/project-list.js";
-import { ProjectType } from "./models/project.js";
-
 console.log("app->");
 
-new ProjectInput();
-new ProjectList(ProjectType.active);
-new ProjectList(ProjectType.finished);
-	
 console.log("app<-");
 
